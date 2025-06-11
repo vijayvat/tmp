918e584667656cdefae5a9f29baabb8963f2b326
vijayvat <vpgvijay@yahoo.com>
2025-06-11 10:03:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,1 @@
-condition = var.azure_attributes != null ? (var.azure_attributes.first_on_demand == null || (var.azure_attributes.first_on_demand != null && var.azure_attributes.first_on_demand > 0)) : true
+condition = var.azure_attributes == null || var.azure_attributes.first_on_demand == null || var.azure_attributes.first_on_demand > 0
